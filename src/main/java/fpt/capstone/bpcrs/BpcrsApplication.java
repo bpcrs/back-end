ed902cc4b9dbff30ebc47b2c4a09055af0ade3f5
thaitranduc <dthai9323@gmail.com>
2020-05-24 18:05:13 +0700

    init project

---
+++ src/main/java/fpt/capstone/bpcrs/BpcrsApplication.java
@@ -0,0 +1,13 @@
+package fpt.capstone.bpcrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpcrsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BpcrsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fpt/capstone/bpcrs/BpcrsApplicationTests.java
@@ -0,0 +1,13 @@
+package fpt.capstone.bpcrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BpcrsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
