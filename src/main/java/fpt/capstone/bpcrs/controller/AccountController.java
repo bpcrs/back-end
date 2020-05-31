8393affeb1de9d593c716102a1faf9d926ebe26a
Thanh Hung <hungpt.se@gmail.com>
2020-05-31 22:26:02 +0700

    change account url and config gg token

--- src/main/java/fpt/capstone/bpcrs/controller/AccountController.java
+++ src/main/java/fpt/capstone/bpcrs/controller/AccountController.java
@@ -3,6 +3,7 @@ package fpt.capstone.bpcrs.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.json.webtoken.JsonWebSignature;
 import fpt.capstone.bpcrs.component.AuthenticationFacade;
 import fpt.capstone.bpcrs.component.GoogleAuthenticator;
 import fpt.capstone.bpcrs.component.JwtTokenProvider;
@@ -25,7 +26,8 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 @RestController
-public class AccountController {
+@RequestMapping("/account")
+public class  AccountController {
 
   private static final ObjectMapper JSON_MAPPER = new ObjectMapper();
 
@@ -39,7 +41,7 @@ public class AccountController {
 
   @Autowired private GoogleIdTokenVerifier googleIdTokenVerifier;
 
-  @GetMapping("/account")
+  @GetMapping
   public ResponseEntity<?> getAccounts() {
     List<Account> accounts = accountService.getAccounts();
     List<AccountResponse> response =
@@ -47,7 +49,7 @@ public class AccountController {
     return ResponseEntity.ok(new ApiResponse<>(true, "", response));
   }
 
-  @PostMapping("/account")
+  @PostMapping
   public ResponseEntity<?> createAccount(@Valid @RequestBody AccountRequest request) {
     try {
       Account account = accountService.createAccount(request);
@@ -58,7 +60,7 @@ public class AccountController {
     }
   }
 
-  @PutMapping("/account/confirm")
+  @PutMapping("/confirm")
   public ResponseEntity<?> confirmAccount(@RequestParam("token") String token) {
     try {
       AccountResponse accountResponse = tokenProvider.getTokenValue(token, AccountResponse.class);
@@ -68,7 +70,7 @@ public class AccountController {
     }
   }
 
-  @PutMapping("/account/{uuid}")
+  @PutMapping("/{uuid}")
   public ResponseEntity<?> updateAccount(
       @PathVariable("uuid") int id, @RequestBody AccountRequest request) {
     try {
@@ -80,7 +82,7 @@ public class AccountController {
     }
   }
 
-  @PatchMapping("/account/{uuid}")
+  @PatchMapping("/{uuid}")
   public ResponseEntity<?> updateAccountStatus(
       @PathVariable("uuid") int id, @RequestParam("active") boolean active) {
     try {
@@ -113,12 +115,10 @@ public class AccountController {
   @PostMapping("/google/login")
   public ResponseEntity<?> loginByGoogle(@Valid @RequestBody StringWrapperRequest wrapper) {
     try {
-
       String token = wrapper.getString();
       if (StringUtils.isEmpty(token)) {
         throw new BadCredentialsException("Invalid login information");
       }
-
       GoogleIdToken googleIdToken = googleIdTokenVerifier.verify(token);
       if (googleIdToken == null) {
         throw new BadRequestException("Google token error");
