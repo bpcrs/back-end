730b7931f659e210a8bea0dc3ac2132bf7a4a4d7
thaitranduc <dthai9323@gmail.com>
2020-05-31 21:57:58 +0700

    config swagger

---
+++ src/main/java/fpt/capstone/bpcrs/config/SpringFoxConfig.java
@@ -0,0 +1,68 @@
+package fpt.capstone.bpcrs.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+import java.util.List;
+
+@Configuration
+@EnableSwagger2
+@Import(springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration.class)
+public class SpringFoxConfig {
+
+  @Bean
+  public Docket apiDocket() {
+    return new Docket(DocumentationType.SWAGGER_2)
+        .select()
+        .apis(RequestHandlerSelectors.any())
+        .paths(PathSelectors.any())
+        .build()
+        .useDefaultResponseMessages(false)
+        .forCodeGeneration(true)
+        .apiInfo(getApiInfo())
+        .securitySchemes(Collections.singletonList(apiKey()))
+        .securityContexts(Collections.singletonList(securityContext()));
+  }
+
+  private ApiInfo getApiInfo() {
+    return new ApiInfo(
+        "Email Template Builder System",
+        "FPT University Capstone project",
+        "0.1",
+        "TERMS OF SERVICE URL",
+        null,
+        "LICENSE",
+        "LICENSE URL",
+        Collections.emptyList());
+  }
+
+  private SecurityContext securityContext() {
+    return SecurityContext.builder()
+        .securityReferences(defaultAuth())
+        .forPaths(PathSelectors.regex("/.*"))
+        .build();
+  }
+
+  private List<SecurityReference> defaultAuth() {
+    final AuthorizationScope authorizationScope =
+        new AuthorizationScope("global", "accessEverything");
+    final AuthorizationScope[] authorizationScopes = new AuthorizationScope[]{authorizationScope};
+    return Collections.singletonList(new SecurityReference("Bearer", authorizationScopes));
+  }
+
+  private ApiKey apiKey() {
+    return new ApiKey("Bearer", "Authorization", "header");
+  }
+}
