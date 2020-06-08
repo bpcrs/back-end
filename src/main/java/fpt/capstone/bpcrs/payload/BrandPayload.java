90d130e0379fe52f37663486394184c65f67ac94
Thanh Hung <hungpt.se@gmail.com>
2020-06-08 13:14:13 +0700

    refactor ModelMapper Brand

--- src/main/java/fpt/capstone/bpcrs/payload/BrandPayload.java
+++ src/main/java/fpt/capstone/bpcrs/payload/BrandPayload.java
@@ -7,18 +7,30 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
 @NoArgsConstructor
 public class BrandPayload {
 
-    public interface Request_CreateBrand_Validate {
-
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class RequestCreateBrand {
+        @NotNull
+        private String name;
+        @NotNull
+        private String logoLink;
+        @Min(1)
+        private int carId;
     }
 
     @EqualsAndHashCode(callSuper = true)
     @Data
     @NoArgsConstructor
-    public static class RequestCreateBrand extends Brand {
-
+    @AllArgsConstructor
+    public static class ResponseCreateBrand extends RequestCreateBrand {
+        @NotNull
+        private int id;
     }
+
 }
