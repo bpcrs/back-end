9f931f4c727d5bd2bea80c1558902f72cac5c4b4
Thanh Hung <hungpt.se@gmail.com>
2020-07-21 05:10:55 +0700

    fix bug

--- src/main/java/fpt/capstone/bpcrs/repository/BookingRepository.java
+++ src/main/java/fpt/capstone/bpcrs/repository/BookingRepository.java
@@ -5,9 +5,11 @@ import fpt.capstone.bpcrs.model.Booking;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface BookingRepository extends JpaRepository<Booking, Integer> {
     List<Booking> findAllByRenter_Id(Integer id);
 
--- src/main/java/fpt/capstone/bpcrs/service/impl/CarServiceImpl.java
+++ src/main/java/fpt/capstone/bpcrs/service/impl/CarServiceImpl.java
@@ -11,16 +11,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.configurationprocessor.json.JSONException;
-import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
-import org.springframework.http.*;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -36,19 +31,19 @@ public class CarServiceImpl implements CarService {
         return carRepository.save(newCar);
     }
 
-    @Override
-    public boolean checkCarVin(Car car) throws JSONException {
-        final String uri = "http://api.carmd.com/v3.0/decode?vin=" + car.getVIN();
-        RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-        headers.add("authorization", "Basic ZDYxYzBlYTgtYjk3YS00ZDBkLTkxMzEtNzQ5MDc2MDFhNzZi");
-        headers.add("partner-token", "54cf08a2145e4c618303d7f77db7fb1f");
-        HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
-        ResponseEntity<String> response = restTemplate.exchange(uri, HttpMethod.GET, entity, String.class);
-        JSONObject jsonObject = new JSONObject(response.getBody());
-        return jsonObject.getJSONObject("message").getString("credentials").equals("valid");
-    }
+//    @Override
+//    public boolean checkCarVin(Car car) throws JSONException {
+//        final String uri = "http://api.carmd.com/v3.0/decode?vin=" + car.getVIN();
+//        RestTemplate restTemplate = new RestTemplate();
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+//        headers.add("authorization", "Basic ZDYxYzBlYTgtYjk3YS00ZDBkLTkxMzEtNzQ5MDc2MDFhNzZi");
+//        headers.add("partner-token", "54cf08a2145e4c618303d7f77db7fb1f");
+//        HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
+//        ResponseEntity<String> response = restTemplate.exchange(uri, HttpMethod.GET, entity, String.class);
+//        JSONObject jsonObject = new JSONObject(response.getBody());
+//        return jsonObject.getJSONObject("message").getString("credentials").equals("valid");
+//    }
 
     @Override
     public Car getCarById(int id) {
@@ -84,9 +79,7 @@ public class CarServiceImpl implements CarService {
 
     @Override
     public List<Car> getAllCarsByOwnerId(int ownerId) {
-//        List<Car> cars = new ArrayList<>();
         List<Car> carList = carRepository.findAllByOwner_Id(ownerId);
-//        cars = BeanUtils.copyProperties();
         return carRepository.saveAll(carList);
     }
 
