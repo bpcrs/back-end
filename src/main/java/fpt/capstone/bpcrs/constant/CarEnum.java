47173148d195a95e8d56af32dad51ede41cec0a0
TienNDSE62860 <44231091+TienNDSE62860@users.noreply.github.com>
2020-07-21 08:27:11 +0700

    add car enum

---
+++ src/main/java/fpt/capstone/bpcrs/constant/CarEnum.java
@@ -0,0 +1,9 @@
+package fpt.capstone.bpcrs.constant;
+
+public enum  CarEnum {
+    AVAILABLE("AVAILABLE"), RENTING("RENTING"), BOOKED("BOOKED"), UNAVAILABLE("UNAVAILABLE");
+
+    CarEnum(String status) {
+
+    }
+}
