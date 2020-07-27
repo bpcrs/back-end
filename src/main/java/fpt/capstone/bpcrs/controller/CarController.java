eee33241eb1d96b72baafeba3cb4d9905fd818c7
Thanh Hung <hungpt.se@gmail.com>
2020-07-27 16:01:10 +0700

    deploy prc

--- src/main/java/fpt/capstone/bpcrs/controller/CarController.java
+++ src/main/java/fpt/capstone/bpcrs/controller/CarController.java
@@ -83,7 +83,7 @@ public class CarController {
         newCar.setBrand(brand);
         newCar.setModel(model);
         newCar.setOwner(accountService.getCurrentUser());
-        newCar.setStatus(CarEnum.UNAVAILABLE);
+        newCar.setStatus(CarEnum.AVAILABLE);
         //check car VIN API (limit 25/month)
 //        try {
 //            if (!carService.checkCarVin(newCar)) {
