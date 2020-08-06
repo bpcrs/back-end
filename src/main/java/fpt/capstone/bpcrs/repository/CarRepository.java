a2d27e1ff55cad10e3ba31eac3c531008b7a46eb
Tran Duc Thai <dthai9323@gmail.com>
2020-08-06 21:46:35 +0700

    check VIN Car existed in BE

--- src/main/java/fpt/capstone/bpcrs/repository/CarRepository.java
+++ src/main/java/fpt/capstone/bpcrs/repository/CarRepository.java
@@ -8,10 +8,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface CarRepository extends JpaRepository<Car, Integer>,  JpaSpecificationExecutor<Car> {
     Page<Car> findAllByOwner_Id(int ownerId, Pageable paging);
     Page<Car> findAllByStatus(CarEnum status, Pageable paging);
+    Car findByVIN(String VIN);
 }
