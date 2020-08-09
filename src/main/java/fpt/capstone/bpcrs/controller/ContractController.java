1668ab3ddaae67bce5661ccbdb33bddd109b3f13
Thanh Hung <hungpt.se@gmail.com>
2020-08-10 03:24:26 +0700

    add renter sign

--- src/main/java/fpt/capstone/bpcrs/controller/ContractController.java
+++ src/main/java/fpt/capstone/bpcrs/controller/ContractController.java
@@ -37,8 +37,12 @@ public class ContractController {
             Account currentUser = accountService.getCurrentUser();
             Booking booking = bookingService.getBookingInformation(id);
             if (booking != null){
-                if (booking.getStatus() != BookingEnum.CONFIRM){
-                    return ResponseEntity.badRequest().body(new ApiResponse<>(false, "Invalid request"));
+
+                if (!bookingService.checkStatusBookingBySM(booking.getStatus(),BookingEnum.RENTER_SIGNED)){
+                    return ResponseEntity.badRequest().body(new ApiResponse<>(false, "Contract must confirm before sign"));
+                }
+                if (booking.getCar().getOwner().equals(currentUser) && booking.getStatus() == BookingEnum.CONFIRM){
+                    return ResponseEntity.badRequest().body(new ApiResponse<>(false, "Renter must signed first"));
                 }
                 DappPayload.ResultChaincode resultChaincode = blockchainService.signingContract(booking,booking.getCar().getOwner().equals(currentUser));
                 if (resultChaincode.isSuccess()) {
