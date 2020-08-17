2b48a05b477b5fb01a077367aa8f9f5f603f810b
Tran Duc Thai <dthai9323@gmail.com>
2020-08-17 19:15:17 +0700

    manage model

--- src/main/java/fpt/capstone/bpcrs/service/ModelService.java
+++ src/main/java/fpt/capstone/bpcrs/service/ModelService.java
@@ -1,16 +1,20 @@
 package fpt.capstone.bpcrs.service;
 
+import fpt.capstone.bpcrs.model.Brand;
 import fpt.capstone.bpcrs.model.Model;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
 public interface ModelService {
 
-    Model createModel(Model newModel);
+    Model createModel(String name);
 
     Model getModelById(int id);
 
-    Model updateModel(Model model, int id);
+    Model updateModel(int id, String name);
 
     List<Model> getAll();
+
+    Page<Model> getAllModelByAdmin(int page, int size);
 }
