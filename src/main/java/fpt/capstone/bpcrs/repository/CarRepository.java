ece6f0c0dac62e5a160a5858ebfbc1fd7cac0353
TienNDSE62860 <44231091+TienNDSE62860@users.noreply.github.com>
2020-08-23 00:36:00 +0700

    filter sort by price asc

--- src/main/java/fpt/capstone/bpcrs/repository/CarRepository.java
+++ src/main/java/fpt/capstone/bpcrs/repository/CarRepository.java
@@ -4,6 +4,7 @@ import fpt.capstone.bpcrs.constant.CarEnum;
 import fpt.capstone.bpcrs.model.Car;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
@@ -16,5 +17,4 @@ public interface CarRepository extends JpaRepository<Car, Integer>,  JpaSpecific
     Page<Car> findAllByStatus(CarEnum status, Pageable paging);
     Car findByVINOrPlateNum(String VIN,String plateNum);
     List<Car> findCarByOwnerIdAndId(int ownerId, int carId);
-
 }
